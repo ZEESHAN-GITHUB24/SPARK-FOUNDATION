1d0c20d879515c29680fff320d46800948b1d730
ZEESHAN-GITHUB24 <77680969+ZEESHAN-GITHUB24@users.noreply.github.com>
2021-09-20 22:54:42 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/* This code is Created by: Mohd Moiz Uddin Zeeshan */
+
+// Function to Open Navigation Bar
+function openMe() {
+	x = document.getElementById("click");
+	x.className = "nav_bar opened";
+}
+
+// Function to Close Navigation Bar
+function closeMe() {
+	x = document.getElementById("click");
+	x.className = "nav_bar closed";
+}
+
+/* This code is Created by: Mohd Moiz Uddin Zeeshan */
